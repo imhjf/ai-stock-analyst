0cc8dda14f50e202714e3b1ff891218720de240a
Rodrick Rendle <30883778+imhjf@users.noreply.github.com>
2025-03-05 11:31:07 +0800

    Update env.dart

--- client/lib/config/env.dart
+++ client/lib/config/env.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 class Env {
   static String get apiBaseUrl {
     // 从环境变量获取，如果没有则使用默认值
-    return Platform.environment['API_BASE_URL'] ?? 'https://stock.imhjf.top';
+    return Platform.environment['API_BASE_URL'] ?? 'http://192.168.0.1:8000';
   }
 
   static bool get isDevelopment {
